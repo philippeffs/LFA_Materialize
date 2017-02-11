969c6c6439ad74ff48afc4d0aea1a83a04338925
Maurício <msvasconcelos.filho@gmail.com>
2017-02-11 02:33:52 -0200

    testando efeitos para fotos da equipe

---
+++ public.php
@@ -0,0 +1,124 @@
+<!--cabeçalho-->
+<?php require('header.php'); ?>
+
+<body>
+
+<!--navbar-->
+<?php require('navbar.php'); ?>
+
+<!--Conteúdo da página-->
+
+      <div class="slider">
+          <ul class="slides">
+            <li>
+              <img src="images/Caroussel/banner1.jpg"> <!-- random image -->
+              <div class="caption center-align">
+                <h3>This is our big Tagline!</h3>
+                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+              </div>
+            </li>
+            <li>
+              <img src="http://lorempixel.com/580/250/nature/2"> <!-- random image -->
+              <div class="caption left-align">
+                <h3>Left Aligned Caption</h3>
+                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+              </div>
+            </li>
+            <li>
+              <img src="http://lorempixel.com/580/250/nature/3"> <!-- random image -->
+              <div class="caption right-align">
+                <h3>Right Aligned Caption</h3>
+                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+              </div>
+            </li>
+            <li>
+              <img src="http://lorempixel.com/580/250/nature/4"> <!-- random image -->
+              <div class="caption center-align">
+                <h3>This is our big Tagline!</h3>
+                <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+              </div>
+            </li>
+          </ul>
+      </div>
+
+
+
+
+<div class="container">
+    <div class="section">
+
+      <!--   Icon Section   -->
+      <div class="row">
+        <div class="col s12 m12 l12">
+          <!--<div class="icon-block">-->
+
+            <h5>Publicações</h5>
+            <p class="light left">
+              Publciações bla bla bla
+
+            </p>
+          <!--</div>-->
+        </div>
+
+    </div>
+      <div class="row">
+        <table class="bordered highlight responsive-table centered">
+        <thead>
+          <tr>
+              <th data-field="id">Name</th>
+              <th data-field="name">Item Name</th>
+              <th data-field="price">Item Price</th>
+              <th data-field="Download">Download</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Alvin</td>
+            <td>Eclair</td>
+            <td>$0.87</td>
+            <td><i class="material-icons small">attachment</i></td>
+          </tr>
+          <tr>
+            <td>Alan</td>
+            <td>Jellybean</td>
+            <td>$3.76</td>
+            <td><i class="material-icons small">attachment</i></td>
+          </tr>
+          <tr>
+            <td>Jonathan</td>
+            <td>Lollipop</td>
+            <td>$7.00</td>
+            <td><i class="material-icons small">attachment</i></td>
+          </tr>
+          <tr>
+            <td>Alvin</td>
+            <td>Eclair</td>
+            <td>$0.87</td>
+            <td><i class="material-icons small">attachment</i></td>
+          </tr>
+          <tr>
+            <td>Alan</td>
+            <td>Jellybean</td>
+            <td>$3.76</td>
+            <td><i class="material-icons small">attachment</i></td>
+          </tr>
+          <tr>
+            <td>Jonathan</td>
+            <td>Lollipop</td>
+            <td>$7.00</td>
+            <td><i class="material-icons small">attachment</i></td>
+          </tr>
+        </tbody>
+      </table>
+      </div>
+  </div>
+</div>
+
+
+<!--rodapé-->
+<?php require ('footer.php'); ?>
+
+
+</body>
+</html>
