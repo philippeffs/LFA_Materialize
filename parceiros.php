0432083065f82ae6b8c9c17072c02bbf59c9acac
Philippe Fernandes <philippeffs@yahoo.com.br>
2017-02-15 22:33:51 -0200

    teste

--- parceiros.php
+++ parceiros.php
@@ -41,13 +41,94 @@
           </ul>
       </div>
 
+
+
+
+  <!--<div id="index-banner" class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <br><br><br><br>
+        <h1 class="header bottom teal-text text-lighten-2">Laboratório de Ferrovias e Asfalto</h1>
+        <div class="row">
+          <h5 class="header col s12 light">Laboratório de Ferrovias e Asfalto</h5>
+        </div>
+
+
+
+      </div>
+    </div>
+    <div class="parallax col s12"><img src="images/banner.jpg"  alt="Laboratório de Ferrovias e Asfalto"></div>
+  </div>-->
+
+
 <div class="container">
     <div class="section">
       <div class="row">
-        <div class="col s12 m9">
-            <h5>Parceiros:</h5>
+        <div class="col s12 m12">
+            <h5>Parceiros</h5>
             <p class="light left">
                 Instituições, Agências de Fomento e Empresas que apoiam nossa iniciativa:
+               
             </p>
 
-            </br>
+
+          
+        </div>
+
+       
+
+    </div>
+        
+  </div>
+    <div class="row" id="thumb">
+              <div class="col s4 m3 l3">
+                    <a href="http://www.em2.ufop.br/" class="z-depth-2 waves-effect hoverable waves-blue-grey">
+                      <img class="responsive-img" src="images/em.jpg" alt="Escola de Minas">
+                    </a>
+                  </div>
+                  <div class="col s4 m3 l3">
+                    <a href="http://www.cnpq.br" class="z-depth-2 waves-effect hoverable waves-blue-grey">
+                      <img class="responsive-img"src="images/cnpq.jpg" alt="CNPq - Conselho Nacional de Desenvolvimento Científico e Tecnológico">
+                    </a>
+                  </div>
+                  <div class="col s4 m3 l3">
+                    <a href="http://www.nugeo.ufop.br/" class="z-depth-2 waves-effect hoverable waves-blue-grey">
+                      <img class="responsive-img"src="images/nugeo.png" alt="Nucleo de Geotecnia da Escola de Minas - NUGEO">
+                    </a>
+                  </div>
+                  <div class="col s4 m3 l3">
+                    <a href="http://www.propec.ufop.br/" class="z-depth-2 waves-effect hoverable waves-blue-grey">
+                      <img class="responsive-img"src="images/propec.jpg" alt="Programa de Pós-Graduação em Engenharia Civil - PROPEC">
+                    </a>
+                  </div>
+              </div>
+      <div class="row">
+                  <div class="col s4 m3 l3">
+                    <a href="http://www.fapemig.br/pt-br/" class="z-depth-2 waves-effect hoverable waves-blue-grey">
+                      <img class="responsive-img"src="images/fapemig.jpg" alt="FAPEMIG - Fundação de Amparo à Pesquisa do Estado de Minas Gerais">
+                    </a>
+                  </div>
+                  <div class="col s4 m3 l3">
+                    <a href="http://2016.gorceixonline.com.br/" class="z-depth-2 waves-effect hoverable waves-blue-grey">
+                      <img class="responsive-img"src="images/fg.jpg" alt="Fundação Gorceix">
+                    </a>
+                  </div>
+                  <div class="col s4 m3 l3">
+                          <a href="http://www.ufop.br/" class="z-depth-2 waves-effect hoverable waves-blue-grey">
+                            <img class="responsive-img" src="images/ufop.jpg" alt="Universidade Federal de Ouro Preto - UFOP">
+                          </a>
+                  </div>
+                        <div class="col s4 m3 l3">
+                          <a href="http://www.ufop.br/" class="z-depth-2 waves-effect hoverable waves-blue-grey">
+                            <img class="responsive-img" src="images/CAPES.jpg" alt="Comissão de Aperfeiçoamento de Pessoal do Nível Superior - Capes">
+                          </a>
+                        </div>  
+              </div>
+</div>
+
+<!--rodapé-->
+<?php require ('footer.php'); ?>
+
+
+  </body>
+</html>
