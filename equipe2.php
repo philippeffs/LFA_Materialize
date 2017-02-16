d0f9c62bbea34732be1957ed1269b0e81a3b66ef
Maurício <msvasconcelos.filho@gmail.com>
2017-02-16 00:41:39 -0200

    testes

---
+++ equipe2.php
@@ -0,0 +1,103 @@
+<!--cabeçalho-->
+<?php require('header.php'); ?>
+
+<body>
+
+<!--navbar-->
+<?php require('navbar.php'); ?>
+
+<!--Conteúdo da página-->
+
+<div class="slider">
+    <ul class="slides">
+      <li>
+        <img src="images/banner.jpg"> <!-- random image -->
+        <div class="caption center-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="http://lorempixel.com/580/250/nature/2"> <!-- random image -->
+        <div class="caption left-align">
+          <h3>Left Aligned Caption</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="http://lorempixel.com/580/250/nature/3"> <!-- random image -->
+        <div class="caption right-align">
+          <h3>Right Aligned Caption</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="http://lorempixel.com/580/250/nature/4"> <!-- random image -->
+        <div class="caption center-align">
+          <h3>This is our big Tagline!</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+    </ul>
+</div>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col s12 m12 l12">
+            <h5>Equipe:</h5>
+            <p class="light left">Alunos de Graduação realizando pesquisa de iniciaçào científica:</p>
+        </div>
+    </div>
+        <div class="row">
+            <div class="col s6">
+                  
+                      <div class="img responsive-img">                
+                        <img src="images/EQUIPE/BCruz.JPG" class="object-fit" width="300" height="900" alt="Avatar">
+                       
+                          <div class=" flow-text center">Bárbara Cruz<br>Estagiária</div>
+                        
+                      </div>
+            </div>
+
+              <div class="col s6">
+                  
+                <div class="teste materialboxed">
+                  <img src="images/EQUIPE/Isadora Castro de Oliveira.JPG" alt="Avatar" class="image1 responsive-img">
+                  <div class="overlay1">
+                    <div class="flow-text center">Isadora Castro de Oliveira<br>Estagiária</div>
+                  </div>
+                  </div>
+                
+              </div>
+        
+        </div>
+         <div class="row">
+                    <div class="col s6">
+                          
+                              <div class="teste">                
+                                <img src="images/EQUIPE/Gilberto - fundador.JPG" alt="Avatar" class="image1 responsive-img">
+                                <div class="overlay1 ">
+                                  <div class="flow-text center">Professor Doutor Gilberto Fernandes<br>
+                                    <p class="light left flow-text">
+                                      
+
+                                    </p>
+                                  </div>
+                                </div>
+                              </div>
+                    </div>
+        </div>
+
+
+
+
+
+</div>  
+
+<!--rodapé-->
+<?php require ('footer.php'); ?>
+
+
+  </body>
+</html>
