9a7936cca434ac9192a6e1afede81f4f6435f6b1
Maurício <msvasconcelos.filho@gmail.com>
2017-02-24 10:15:27 -0300

    ajustando fotos do laboratorio

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@
   <!--  Scripts-->
  
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
-  
+  <script src="js/lightbox.min.js"></script>
 
 
 
