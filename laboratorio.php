d50ba5403653b883cd159c40bfbabd4c23c329d6
Maurício <msvasconcelos.filho@gmail.com>
2017-02-24 18:49:09 -0300

    ajustando site em geral

--- laboratorio.php
+++ laboratorio.php
@@ -27,64 +27,67 @@
 
 
 
-  <div class="container">
-      <div class="section">
-        <div class="row">
-          <div class="col s12 m12 l12">
-              <h5>O Laboratório</h5>
-              <p class="light left">O laboratório foi projetado em um espaço interno de 110 metros quadrados e possui uma área externa de 150 metros quadrados para desenvolvimentos de pesquisas de graduação e pós-graduação. É equipado para atender demandas relacionadas a ensaios da Metodologia Marshall de acordo com a norma da Associação Brasileira de Normas Técnicas (ABNT) NBR 12891, além de possuir todo o aparato para realização de ensaios de agregados para pavimentação e de ligantes asfálticos. Também está equipado para a caracterização tecnológica de agregados para lastro ferroviário conforme a ABNT NBR 5564 e para a adequação de materiais alternativos como escórias siderúrgicas e rejeitos de mineração tanto para utilização em pavimentos rodoviários quanto para os ferroviários.</p>
-          </div>
+ <div class="container">
+        <div class="section">
 
-      </div>
+          <!--   Icon Section   -->
+          <div class="row">
+            <div class="col s12 m12 l12">
+              <!--<div class="icon-block">-->
 
-    </div>
-  </div>
+                <h5>O Laboratório</h5>
+                <p class="light left">O laboratório foi projetado em um espaço interno de 110 metros quadrados e possui uma área externa de 150 metros quadrados para desenvolvimentos de pesquisas de graduação e pós-graduação. É equipado para atender demandas relacionadas a ensaios da Metodologia Marshall de acordo com a norma da Associação Brasileira de Normas Técnicas (ABNT) NBR 12891, além de possuir todo o aparato para realização de ensaios de agregados para pavimentação e de ligantes asfálticos. Também está equipado para a caracterização tecnológica de agregados para lastro ferroviário conforme a ABNT NBR 5564 e para a adequação de materiais alternativos como escórias siderúrgicas e rejeitos de mineração tanto para utilização em pavimentos rodoviários quanto para os ferroviários.</p>
+              <!--</div>-->
+            </div>
 
-    <div class="container">
+        </div>
         <div class="row" id="galerialab">
-      <div class="col s12 m4 l3">
-            <a class="example-image-link" href="images/LABORATORIO/Selecionada1.jpg" data-lightbox="lightbox" data-title="Bla bla bla">
-            <img class="example-image" src="images/LABORATORIO/Selecionada1.jpg" alt="bla bla bla"/></a>
+        <div class="col s6 m4 l3">
+              <a class="example-image-link" href="images/LABORATORIO/Selecionada1.jpg" data-lightbox="lightbox" data-title="Bla bla bla">
+              <img class="example-image responsive-img" src="images/LABORATORIO/Selecionada1.jpg" alt="bla bla bla"/></a>
+        </div>
+        <div class="col s6 m4 l3">
+              <a class="example-image-link" href="images/LABORATORIO/Selecionada11.jpg" data-lightbox="lightbox" data-title="Bla bla bla">
+              <img class="example-image responsive-img" src="images/LABORATORIO/Selecionada11.jpg" alt="bla bla bla"/></a>
+        </div>
+        <div class="col s6 m4 l3">
+              <a class="example-image-link" href="images/LABORATORIO/Selecionada14.jpg" data-lightbox="lightbox" data-title="Bla bla bla">
+              <img class="example-image responsive-img" src="images/LABORATORIO/Selecionada14.jpg" alt="bla bla bla"/></a>
+        </div>
+        <div class="col s6 m4 l3">          
+              <a class="example-image-link" href="images/LABORATORIO/Selecionada16.jpg" data-lightbox="lightbox" data-title="Bla bla bla">
+              <img class="example-image responsive-img" src="images/LABORATORIO/Selecionada16.jpg" alt="bla bla bla"/></a>
+        </div>
+        <div class="col s6 m4 l3">          
+              <a class="example-image-link" href="images/LABORATORIO/selecionada20.jpg" data-lightbox="lightbox" data-title="Bla bla bla">
+              <img class="example-image responsive-img" src="images/LABORATORIO/selecionada20.jpg" alt="bla bla bla"/></a>
+        </div>
+        <div class="col s6 m4 l3">
+        <a class="example-image-link" href="images/LABORATORIO/Selecionada_CBR.jpg" data-lightbox="lightbox" data-title="Bla bla bla">
+        <img class="example-image responsive-img" src="images/LABORATORIO/Selecionada_CBR.jpg" alt="bla bla bla"/></a>
+        </div>
+        <div class="col s6 m4 l3">
+        <a class="example-image-link" href="images/LABORATORIO/Selecionada_Compactacao.jpg" data-lightbox="lightbox" data-title="Bla bla bla">
+        <img class="example-image responsive-img" src="images/LABORATORIO/Selecionada_Compactacao.jpg" alt="bla bla bla"/></a>
+        </div>
+        <div class="col s6 m4 l3">
+        <a class="example-image-link" href="images/LABORATORIO/Selecionada_Geogaude.jpg" data-lightbox="lightbox" data-title="Bla bla bla">
+        <img class="example-image responsive-img" src="images/LABORATORIO/Selecionada_Geogaude.jpg" alt="bla bla bla"/></a>
+        </div> 
+        <div class="col s6 m4 l3">
+        <a class="example-image-link" href="images/LABORATORIO/selecionada_mistura.jpg" data-lightbox="lightbox" data-title="Bla bla bla">
+        <img class="example-image responsive-img" src="images/LABORATORIO/selecionada_mistura.jpg" alt="bla bla bla"/></a>
+        </div> 
+        <div class="col s6 m4 l3">     
+        <a class="example-image-link" href="images/LABORATORIO/Selecionada_Ensaio_Penetracao.jpg" data-lightbox="lightbox" data-title="Bla bla bla">
+        <img class="example-image responsive-img" src="images/LABORATORIO/Selecionada_Ensaio_Penetracao.jpg" alt="bla bla bla"/></a>
+        </div>            
       </div>
-      <div class="col s12 m4 l3">
-            <a class="example-image-link" href="images/LABORATORIO/Selecionada11.jpg" data-lightbox="lightbox" data-title="Bla bla bla">
-            <img class="example-image" src="images/LABORATORIO/Selecionada11.jpg" alt="bla bla bla"/></a>
-      </div>
-      <div class="col s12 m4 l3">
-            <a class="example-image-link" href="images/LABORATORIO/Selecionada14.jpg" data-lightbox="lightbox" data-title="Bla bla bla">
-            <img class="example-image" src="images/LABORATORIO/Selecionada14.jpg" alt="bla bla bla"/></a>
-      </div>
-      <div class="col s12 m4 l3">          
-            <a class="example-image-link" href="images/LABORATORIO/Selecionada16.jpg" data-lightbox="lightbox" data-title="Bla bla bla">
-            <img class="example-image" src="images/LABORATORIO/Selecionada16.jpg" alt="bla bla bla"/></a>
-      </div>
-      <div class="col s12 m4 l3">          
-            <a class="example-image-link" href="images/LABORATORIO/selecionada20.jpg" data-lightbox="lightbox" data-title="Bla bla bla">
-            <img class="example-image" src="images/LABORATORIO/selecionada20.jpg" alt="bla bla bla"/></a>
-      </div>
-      <div class="col s12 m4 l3">
-      <a class="example-image-link" href="images/LABORATORIO/Selecionada_CBR.jpg" data-lightbox="lightbox" data-title="Bla bla bla">
-      <img class="example-image" src="images/LABORATORIO/Selecionada_CBR.jpg" alt="bla bla bla"/></a>
-      </div>
-      <div class="col s12 m4 l3">
-      <a class="example-image-link" href="images/LABORATORIO/Selecionada_Compactacao.jpg" data-lightbox="lightbox" data-title="Bla bla bla">
-      <img class="example-image" src="images/LABORATORIO/Selecionada_Compactacao.jpg" alt="bla bla bla"/></a>
-      </div>
-      <div class="col s12 m4 l3">
-      <a class="example-image-link" href="images/LABORATORIO/Selecionada_Geogaude.jpg" data-lightbox="lightbox" data-title="Bla bla bla">
-      <img class="example-image" src="images/LABORATORIO/Selecionada_Geogaude.jpg" alt="bla bla bla"/></a>
-      </div> 
-      <div class="col s12 m4 l3">
-      <a class="example-image-link" href="images/LABORATORIO/selecionada_mistura.jpg" data-lightbox="lightbox" data-title="Bla bla bla">
-      <img class="example-image" src="images/LABORATORIO/selecionada_mistura.jpg" alt="bla bla bla"/></a>
-      </div> 
-      <div class="col s12 m4 l3">     
-      <a class="example-image-link" href="images/LABORATORIO/Selecionada_Ensaio_Penetracao.jpg" data-lightbox="lightbox" data-title="Bla bla bla">
-      <img class="example-image" src="images/LABORATORIO/Selecionada_Ensaio_Penetracao.jpg" alt="bla bla bla"/></a>
-      </div>       
     </div>
-  </div>
 
+
+      </div>
+    </div>
 
 
   <!--rodapé-->
--- navbar.php
+++ navbar.php
@@ -1,35 +1,35 @@
 
-<div class="navbar-fixed">
-  <nav class="blue-grey darken-1 z-depth-4" role="navigation">
-    <div class="nav-wrapper container">
-      <a id="logo-container" href="#" class="brand-logo">LABFA</a>
-      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons ">menu</i></a>
-  
-       <ul class="right hide-on-med-and-down">
-        <li><a href="index.html">Página inicial</a></li>
-        <li><a href="laboratorio.html">Laboratório</a></li>
-        <li><a href="equipe.html">Equipe</a></li>
-        <li><a href="lnpesquisa.html">Linhas de Pesquisa</a></li>
-        <li><a href="public.html">Publicações</a></li>
-        <li><a href="parceiros.html">Parceiros</a></li>
-        <li><a href="contato.html">Contato</a></li>
-      </ul>
-    </div>
-      <ul id="nav-mobile" class="side-nav">
-          <li class="bold"><a href="index.html" class="waves-effect waves-light waves-grey">Página inicial</a></li>
-          <li><div class="divider"></div></li>
-          <li class="bold"><a href="laboratorio.html" class="waves-effect waves-light waves-grey">Laboratório</a></li>
-          <li><div class="divider"></div></li>
-          <li class="bold"><a href="equipe.html" class="waves-effect waves-light waves-grey">Equipe</a></li>
-          <li><div class="divider"></div></li>
-          <li class="bold"><a href="lnpesquisa.html" class="waves-effect waves-light waves-grey">Linhas de Pesquisa</a></li>
-          <li><div class="divider"></div></li>
-          <li class="bold"><a href="public.html" class="waves-effect waves-light waves-grey">Publicações</a></li>
-          <li><div class="divider"></div></li>
-          <li class="bold"><a href="#!" class="waves-effect waves-light waves-grey">Parceiros</a></li>          
-          <li><div class="divider"></div></li>
-          <li class="bold"><a href="contato.html" class="waves-effect waves-light waves-grey">Contato</a></li>
-      </ul>
+    <div class="navbar-fixed">
+      <nav class="blue-grey darken-1 z-depth-4" role="navigation">
+        <div class="nav-wrapper container">
+          <a id="logo-container" href="#" class="brand-logo">LABFA</a>
+          <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons ">menu</i></a>
+      
+          <ul class="right hide-on-med-and-down">
+            <li><a href="index.html">Página inicial</a></li>
+            <li><a href="laboratorio.html">Laboratório</a></li>
+            <li><a href="equipe.html">Equipe</a></li>
+            <li><a href="lnpesquisa.html">Linhas de Pesquisa</a></li>
+            <li><a href="public.html">Publicações</a></li>
+            <li><a href="parceiros.html">Parceiros</a></li>
+            <li><a href="contato.html">Contato</a></li>
+          </ul>
+        </div>
+          <ul id="nav-mobile" class="side-nav">
+              <li class="bold"><a href="index.html" class="waves-effect waves-light waves-grey">Página inicial</a></li>
+              <li><div class="divider"></div></li>
+              <li class="bold"><a href="laboratorio.html" class="waves-effect waves-light waves-grey">Laboratório</a></li>
+              <li><div class="divider"></div></li>
+              <li class="bold"><a href="equipe.html" class="waves-effect waves-light waves-grey">Equipe</a></li>
+              <li><div class="divider"></div></li>
+              <li class="bold"><a href="lnpesquisa.html" class="waves-effect waves-light waves-grey">Linhas de Pesquisa</a></li>
+              <li><div class="divider"></div></li>
+              <li class="bold"><a href="public.html" class="waves-effect waves-light waves-grey">Publicações</a></li>
+              <li><div class="divider"></div></li>
+              <li class="bold"><a href="#!" class="waves-effect waves-light waves-grey">Parceiros</a></li>          
+              <li><div class="divider"></div></li>
+              <li class="bold"><a href="contato.html" class="waves-effect waves-light waves-grey">Contato</a></li>
+          </ul>
 
-  </nav>
-</div>
+      </nav>
+    </div>
